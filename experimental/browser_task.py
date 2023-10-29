798f88d9c33c1fa5da797afdf0c293773c47cd53
Walmqrt <46291857+Zenqrt@users.noreply.github.com>
2023-10-29 16:46:52 -0400

    Remove all python code

--- experimental/browser_task.py
+++
@@ -1,4 +0,0 @@
-import os
-
-
-os.system("start opera https://www.youtube.com/watch?v=dQw4w9WgXcQ")
--- tests/test_task.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-
-from task import BrowserTask
-
-
-class TestBrowserTask(TestCase):
-    def test__start_default_browser(self):
-        browser_task = BrowserTask("google", "https://www.google.com")
-        self.assertEqual(browser_task.execution_command, "start https://www.google.com")
